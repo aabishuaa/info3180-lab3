c034cf26d9e0c36de51089c3d405d9b4034ebbe9
aabishuaa <abishuasjohnson@gmail.com>
2025-02-28 19:33:45 -0500

    INFO3180 Lab #3 completed with all necessary exercises finished

--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from flask_mail import Message
+from app import app, mail
+from .forms import ContactForm
+
 
 
 ###
@@ -15,7 +19,27 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Abishua Johnson")
+
+@app.route('/contact/', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        subject = form.subject.data
+        message = form.message.data
+
+        msg = Message(subject,
+                      sender=(name, email),
+                      recipients=["to@example.com"])
+        msg.body = message
+
+        mail.send(msg)
+        flash("Your message has been sent successfully!", "success")
+        return redirect(url_for('home'))
+    
+    return render_template('contact.html', form=form)
 
 
 ###
